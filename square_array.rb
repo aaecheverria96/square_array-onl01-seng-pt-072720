7defbb9de191256c69d26ec10d2fca435f3ec0ad
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-04 03:00:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,5 @@
 def square_array(array)
-new_numbers = []
-array.each do |i|
-   i ** 2 == array.push
-   new_numbers == array.push
+new_numbers.push(array.each do |i| i**2)
    return new_numbers
    end
 end
